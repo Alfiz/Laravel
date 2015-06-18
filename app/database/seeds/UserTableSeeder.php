38e2f8655469d611075348dd5e0516ee7d1945d9
Alfredo <jajimz@gmail.com>
2015-06-17 21:10:51 -0500

    Agregamos pantallas y colocamos la clase active

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,14 +6,13 @@ class UserTableSeeder extends Seeder {
 	public function run()
 	{
 		User::create([
-			'first_name' => 'Alicia',
-			'last_name'  => 'Guzman',
-			'username'   => 'alicia.guzman',
+			'first_name' => 'Ezequiel',
+			'last_name'  => 'Gonzalez Garcia',
+			'username'   => 'ezeezegg',
 			'email'      => 'ezeezegg@gmail.com',
 			'password'   => 'admin'
 			//'password'   =>  Hash::make('admin')
 			]);
 		}
-		
-		
+
 	}
